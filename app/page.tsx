14c8a53783766431cdf0cf95bc1345df8ffcb222
Skandan Senthil Nathan <skandansn@protonmail.com>
2024-07-31 09:29:00 +0530

    main / home as rafce

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 'use client'
-import Image from 'next/image'
-import Link from 'next/link'
 import AIToolDivider from './components/AIToolDivider/AIToolDivider'
 import NavBar from './components/NavBar'
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main>
+//       <NavBar/>
+//       <AIToolDivider/>
+//     </main>
+//   )
+// }
+
+import React from 'react'
+
+const Home = () => {
   return (
     <main>
       <NavBar/>
       <AIToolDivider/>
-    </main>
-  )
+    </main>  
+         )
 }
+
+export default Home
