c3f55b9611dd0cd86eaf9d43b6b55b0c3ae9af03
Skandan Senthil Nathan <skandansn@protonmail.com>
2024-07-31 09:55:45 +0530

    client com and uncom

--- app/components/AIToolDivider/InputReceiver/InputReceiver.tsx
+++ app/components/AIToolDivider/InputReceiver/InputReceiver.tsx
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 import React, { useState, useEffect } from 'react'
 // import { cookies } from 'next/headers'
 import GenerateResumeButton from './GenerateResumeButton';
